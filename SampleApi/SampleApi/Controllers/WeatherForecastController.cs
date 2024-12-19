83105c0973cb5e7af770b538d3689c5e470b29b5
sagar7876 <51994414+sagar7876@users.noreply.github.com>
2024-12-19 13:25:11 +0530

    Update WeatherForecastController.cs

--- SampleApi/SampleApi/Controllers/WeatherForecastController.cs
+++ SampleApi/SampleApi/Controllers/WeatherForecastController.cs
@@ -29,5 +29,11 @@ namespace SampleApi.Controllers
             })
             .ToArray();
         }
+
+          [HttpGet(Name = "GetTest")]
+        public string GetTest()
+        {
+            return "Container App Deployment is working";
+        }
     }
 }
